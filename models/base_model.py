3117d868ebad86447c072e3f45692f4437db0fca
Aden1ke <sarahogunlalu@.com>
2023-10-12 05:55:17 +0100

    serilization and deserilization

--- models/base_model.py
+++ models/base_model.py
@@ -26,9 +26,10 @@ class BaseModel:
                         self.updated_at, "%Y-%m-%dT%H:%M:%S.%f"
                         )
         else:
-            self.id = str(uuid.uuid4())
+            self.id = str(uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
         """print the attributes in the module"""
@@ -37,6 +38,7 @@ class BaseModel:
     def save(self):
         """Update the 'updated_at' attribute with the current date and time."""
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """returns a dictionary containing all keys/values of __dict__"""
