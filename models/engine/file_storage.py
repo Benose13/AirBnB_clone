fe9a5de81db11263690b0b5504124b71de9ad225
jaymine <okoegualebenjamin38@gmail.com>
2023-10-15 13:58:13 -0700

    task5

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """Class FileStorage"""
-import os
 import json
 from models.base_model import BaseModel
 from models.user import User
@@ -12,28 +11,29 @@ class FileStorage:
 
     def all(self):
         """return __objects"""
-        return self.__objects
+        return FileStorage.__objects
 
     def new(self, obj):
         """ put key(class name.id) and obj value in obj dic"""
         key = f"{obj.__class__.__name__}.{obj.id}"
-        self.__objects[key] = obj
+        FileStorage.__objects[key] = obj
 
     def save(self):
         """serialize a dic"""
         data = {}
-        for key, obj in self.__objects.items():
+        for key, obj in FileStorage.__objects.items():
             data[key] = obj.to_dict()
-        with open(self.__file_path, "w") as file:
+        with open(FileStorage.__file_path, "w") as file:
             json.dump(data, file)
 
     def reload(self):
         """deserialize json"""
-        if os.path.isfile(self.__file_path):
-            with open(self.__file_path, "r") as file:
-                data = json.load(file)
-                for key, value in data.items():
-                    class_name, obj_id = key.split('.')
-                    class_ = globals()[class_name]
-
-                    self.__objects[key] = class_(**value)
+        try:
+            with open(FileStorage.__file_path) as f:
+                objdict = json.load(f)
+                for o in objdict.values():
+                    cls_name = o["__class__"]
+                    del o["__class__"]
+                    self.new(eval(cls_name)(**o))
+        except FileNotFoundError:
+            return
