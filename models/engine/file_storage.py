ef6318dd7a1f7c01cbb4f86f0819fc8554b37001
Aden1ke <sarahogunlalu@.com>
2023-10-16 04:24:29 +0100

    import

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -6,6 +6,12 @@ This module is in charge of the storage of the classes
 import json
 from models.base_model import BaseModel
 from models.user import User
+from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
 from os import path
 
 
