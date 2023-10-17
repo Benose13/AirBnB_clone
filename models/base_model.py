150729b90a49a7432fbdb7b9822bb779d7fd8812
jaymine <okoegualebenjamin38@gmail.com>
2023-10-17 10:35:05 -0700

    gff

--- models/base_model.py
+++ models/base_model.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
-"""Defines the BaseModel class"""
+
+import uuid
 import models
-from uuid import uuid4
 from datetime import datetime
 
 
-class BaseModel:
-    """Defines BaseModel as the base for other classes."""
+class BaseModel():
+    """
+    Defining a super class BaseModel which other classes
+    can inherit these common attributes and methods
+    and have the functionality described.
+
+
+    """
     def __init__(self, *args, **kwargs):
         """
         Initialize BaseModel class
-
-        Args:
-            *args: Unused.
-            **kwargs (dict): Key/value pairs of attributes.
-
         """
         if kwargs:
             if kwargs["__class__"] == self.__class__.__name__:
@@ -28,22 +29,29 @@ class BaseModel:
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            self.updated_at = self.created_at
             models.storage.new(self)
 
-    def __str__(self):
-        """print the attributes in the module"""
-        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
-
     def save(self):
-        """Update the 'updated_at' attribute with the current date and time."""
+        """
+        Updates the public instance attribute
+        'updated_at' with the current datetime.
+        """
         self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
-        """returns a dictionary containing all keys/values of __dict__"""
+        """
+        Returns a dictionary representation of the object's attributes.
+        """
         dict_cp = self.__dict__.copy()
         dict_cp['__class__'] = self.__class__.__name__
         dict_cp['created_at'] = self.created_at.isoformat()
         dict_cp['updated_at'] = self.updated_at.isoformat()
         return dict_cp
+
+    def __str__(self):
+        """
+        Return the string representation of the class for the user.
+        """
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
